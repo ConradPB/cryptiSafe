8e7d58cfd7a3ef96e1935794c8909ea3af2d8686
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-05-20 00:17:42 +0300

    updated index, updated project structure

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,12 @@
-console.log(
-  'Hello World! Am back with a bang!'
-);
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
