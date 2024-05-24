0153133b5cd47c7445e552ae60602d76d1a14ade
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2024-05-24 21:48:15 +0300

    added user model

---
+++ backend/src/models/user.ts
@@ -0,0 +1,19 @@
+import { Schema, model } from 'mongoose';
+
+interface IUser {
+  name: string;
+  email: string;
+  password: string;
+  date: Date;
+}
+
+const UserSchema = new Schema<IUser>({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  date: { type: Date, default: Date.now }
+});
+
+const User = model<IUser>('User', UserSchema);
+
+export default User;
